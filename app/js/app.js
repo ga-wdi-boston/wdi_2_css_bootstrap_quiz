a9477e214a16b735e92a7aadaa58422ff99a3ff5
christobeck <ga@christobeck.com>
2015-05-08 16:21:34 -0400

    quiz

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,10 @@
 // your code here
+
+
+    var templatingFunction = Handlebars.compile($('#boxy-index').html());
+    var result = templatingFunction({boxy: boxy});
+
+    $('#content').html(result);
 $(document).ready(function(){
   $('.boxy').click(function(){
     $(this).remove();
